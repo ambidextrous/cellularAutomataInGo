c710bdc59cabea0ffcf98003ace7ae5abefbe3a8
Aidan <butleraidan@gmail.com>
2017-04-15 16:54:37 +0200

    PrintWorld functionality.

--- TestWorld.go
+++ TestWorld.go
@@ -1,30 +1,29 @@
 package main
 
-import "time"
+//import "time"
 import "fmt"
 
 // Main struct in which all other simulation elements are located, contains a two-dimensional array of nodes
 type world struct {
 	nodes         [][]node
-	height        int
-	width         int
+	width        int
+	height         int
 	oneSpaceMoves [][]int
 	worldType     string
 }
 
+
 // Returns a string representation of a given world's state
-func (w world) String() string {
+func (w world) printWorld() {
 	worldState := ""
-	for i := 0; i < w.width; i++ {
-		for j := 0; j < w.height; j++ {
+	for i := 0; i < w.height; i++ {
+		for j := 0; j < w.width; j++ {
 			n := w.nodes[i][j]
 			worldState += n.resident.species
-			//string nodeState = w.nodes[i][j]
-			//worldState += nodeState
 		}
 		worldState += "\n"
 	}
-	return worldState
+	fmt.Println(worldState)
 }
 
 // Struct which can be inhabitted by a creature; linked to 0 or 1 creatures by a channel
@@ -34,16 +33,6 @@ type node struct {
 	resident          creature
 	neighbouringNodes []node        // Slice needs to be made prior to assignment
 	channelToResident chan<- string // Channel needs to be made prior to assignment
-}
-
-// Returns string representation of a given node
-func (n node) String() string {
-	emptyNodeSymbol := "-"
-	pointerToNode := &n.resident // Pointer
-	if pointerToNode == nil {
-		return emptyNodeSymbol
-	}
-	return n.resident.species
 }
 
 // Struct representing a node inhabitant, has a species name, a lifetime and a fitness value; linked to a node by a channel
@@ -56,39 +45,53 @@ type creature struct {
 }
 
 // Creates a new world struct
-func createWorld(height int, width int, worldType string) world {
-	w := world{nodes: nil, height: height, width: width, oneSpaceMoves: nil, worldType: worldType}
-	w = addNodes(w)
-	fmt.Println("w.nodes = ", w.nodes)
+func createWorld(width int, height int, worldType string) world {
+	w := world{nodes: nil, width: width, height: height, oneSpaceMoves: nil, worldType: worldType}
+	wPointer := &w
+	addNodes(wPointer)
+	addAntiCreature(wPointer)
 	return w
 }
 
+// Adds an anti-creature to each node in a given world
+func addAntiCreature(w *world) {
+	for i := 0; i < w.height; i++ {
+		for j := 0; j < w.width; j++ {
+			nPointer := &w.nodes[i][j]
+			c := creature{species: "-", lifeTime: 1000000000000, fitness: 0.0, node: nPointer}
+			nPointer.resident = c
+		}
+	}
+}
+
 // Adds empty nodes to a given  world
-func addNodes(w world) world {
+func addNodes(w *world) {
 	// Creates empty 2D node slices
-	w.nodes = make([][]node, w.width)
-	for i := 0; i < w.width; i++ {
-		w.nodes[i] = make([]node, w.height)
+	w.nodes = make([][]node, w.height)
+	for i := 0; i < w.height; i++ {
+		w.nodes[i] = make([]node, w.width)
 	}
 	// Populates slices with empty nodes
-	for i := 0; i < w.width; i++ {
-		for j := 0; j < w.height; j++ {
+	for i := 0; i < w.height; i++ {
+		for j := 0; j < w.width; j++ {
 			ch := make(chan string)
 			n := node{horiz: i, vert: j, neighbouringNodes: nil, channelToResident: ch}
 			w.nodes[i][j] = n
 		}
 	}
-	return w
 }
 
 // Main programme function
 func main() {
-	height := 10
 	width := 30
+	height := 10
 	worldType := "roundWorld"
-	w := createWorld(height, width, worldType)
-	for {
-		time.Sleep(500 * time.Millisecond)
-		fmt.Println(w)
-	}
+	w := createWorld(width, height, worldType)
+
+	w.printWorld()
+	//fmt.Println(w)
+	//for {
+	//	time.Sleep(500 * time.Millisecond)
+	//	fmt.Println(w)
+	//}
 }
